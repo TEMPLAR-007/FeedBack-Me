4d233513bd7df24bd6f3bb6a0714bee980ab87ce
TEMPLAR-007 <arefin.khan8364@gmail.com>
2022-04-04 05:24:12 +0600

    Nav-Bar done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand className='fs-4 fw-bolder' href="/"><span className='text-danger'>FeedBack</span>Me</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse className='justify-content-end ' id="basic-navbar-nav">
+                        <Nav className="gap-3">
+                            <CustomLink to="/home">Home</CustomLink>
+                            <CustomLink to="/reviews">Reviews</CustomLink>
+                            <CustomLink to="/about">About</CustomLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
